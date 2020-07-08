194499f829c8102295d4bc39704989fb782f874a
Chris Stark <starkfree@gmail.com>
2020-07-08 03:57:24 -0400

    feat: adds responsiveness to search

--- components/search/Search.vue
+++ components/search/Search.vue
@@ -1,48 +1,152 @@
 <template>
     <div class="biogrid-search align-center">
-        <v-sheet
-            color="white"
-            class="pa-2 black--text"
-            rounded
-            elevation="8"
+        <v-row no-gutters>
+            <v-col
+                lg="11"
+                md="9"
+                sm="9"
+                cols="9"
+                offset-lg="0"
+                offset-md="1"
+                offset-sm="1"
+                offset="1"
+            >
+                <v-sheet
+                    color="white"
+                    class="pa-2 black--text rounded-l-lg"
+                    elevation="8"
+                >
+                    <v-row no-gutters>
+                        <v-col
+                            lg="8"
+                            md="8"
+                            sm="8"
+                            cols="7"
+                        >
+                            <v-text-field
+                                :autofocus="true"
+                                solo
+                                flat
+                                light
+                                color="white"
+                                placeholder="What are you looking for?"
+                                hide-details="auto"
+                            />
+                        </v-col>
+                        <v-col
+                            lg="4"
+                            md="4"
+                            sm="4"
+                            cols="5"
+                            class="black--text"
+                        >
+                            <v-select
+                                v-model="searchType"
+                                outlined
+                                dense
+                                light
+                                hide-details="auto"
+                                prepend-icon="mdi-arrow-right"
+                                class="mt-1 searchTypeSelect"
+                                :items="searchOptions"
+                            />
+                        </v-col>
+                    </v-row>
+                </v-sheet>
+            </v-col>
+            <v-col
+                lg="1"
+                md="1"
+                sm="1"
+                cols="1"
+            >
+                <v-btn
+                    color="accent"
+                    width="100%"
+                    height="100%"
+                    class="rounded-0 rounded-r-lg rounded-r-md rounded-r-sm"
+                >
+                    <v-icon x-large>
+                        mdi-chevron-double-right
+                    </v-icon>
+                </v-btn>
+            </v-col>
+        </v-row>
+        <v-row
+            v-if="showOrganismSelect()"
+            no-gutters
+            class="mt-2"
         >
-            <v-row no-gutters>
-                <v-col
-                    lg="7"
+            <v-col
+                lg="1"
+                md="1"
+                sm="1"
+                offset-sm="2"
+                offset-md="2"
+                offset-lg="2"
+                class="hidden-xs-only"
+            >
+                <v-icon
+                    x-large
+                    color="warning"
+                    class="mt-1"
                 >
-                    <v-text-field
-                        :autofocus="true"
-                        solo
-                        flat
-                        light
-                        color="white"
-                        placeholder="What are you looking for?"
-                        hide-details="auto"
-                    />
-                </v-col>
-                <v-col
-                    lg="3"
-                    class="black--text"
-                >
-                    Select Box
-                </v-col>
-                <v-col
-                    lg="2"
-                >
-                    BTN
-                </v-col>
-            </v-row>
-        </v-sheet>
+                    mdi-subdirectory-arrow-right
+                </v-icon>
+            </v-col>
+            <v-col
+                lg="6"
+                md="6"
+                sm="6"
+                offset-sm="0"
+                offset-md="0"
+                offset-lg="0"
+                cols="8"
+                offset="1"
+            >
+                <v-select
+                    v-model="searchType"
+                    solo
+                    light
+                    hide-details="auto"
+                    class="mt-1 searchTypeSelect"
+                    :items="searchOptions"
+                />
+            </v-col>
+        </v-row>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import { SelectOption } from '@/utilities/interfaces.ts'
 
 @Component
 export default class Search extends Vue {
+    private searchType: string = 'pg';
+    private searchOptions: SelectOption[] = [
+        { value: 'pg', text: 'Proteins & Genes' },
+        { value: 'chem', text: 'Chemical Compounds' },
+        { value: 'pub', text: 'Publications' },
+        { value: 'go', text: 'GO Terms' }
+    ];
+
+    private showOrganismSelect () {
+        if (this.searchType === 'pg' || this.searchType === 'go') {
+            return true
+        }
+
+        return false
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+    ::v-deep .v-select__selection {
+        line-height: 22px !important;
+    }
+    .testMe {
+        background-color: salmon;
+        color: orange;
+    }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,31 @@
 <template>
     <v-container fluid class="index-page pa-0">
         <v-sheet color="secondary">
-            <v-container class="pa-0">
+            <v-container class="pa-0 constrainSize">
                 <v-card color="secondary" dark class="pa-0 ma-0 white--text text-center" flat rounded="0">
                     <v-img
                         src="/images/biogrid_logo.png"
                         contain
                         position="center center"
                         max-height="100"
-                        class="mr-12"
+                        class="mr-12 hidden-sm-and-down"
                     />
                     <h1 class="accent--text">
-                        Welcome to the Biological General Repository for Interaction Datasets
+                        Welcome to the <span class="hidden-sm-and-down">Biological</span> General Repository for Interaction Datasets
                     </h1>
-                    <div class="subtitle-1 grey--text text--lighten-4">
-                        BioGRID is an open access, interaction database with data compiled through comprehensive literature curation efforts. All data are freely provided via our search index and available for download in standardized formats.
+                    <div class="subtitle-1 grey--text text--lighten-4 ml-3 mr-3">
+                        BioGRID is an open access, molecular interaction database with data compiled through comprehensive literature curation efforts. All data are freely provided via our search index and available for download in multiple standardized formats.
                     </div>
                     <v-row no-gutters class="mt-7 mb-7">
                         <v-col
                             lg="8"
+                            md="10"
+                            sm="12"
+                            cols="12"
+                            offset="0"
                             offset-lg="2"
+                            offset-md="1"
+                            offset-sm="0"
                         >
                             <Search />
                         </v-col>
@@ -27,7 +33,9 @@
                     <v-row no-gutters>
                         <v-col
                             lg="10"
+                            md="10"
                             offset-lg="1"
+                            offset-md="1"
                         >
                             <HighlightCards :items="highlightItems" />
                         </v-col>
@@ -36,7 +44,7 @@
             </v-container>
         </v-sheet>
 
-        <v-container>
+        <v-container class="constrainSize">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et commodo purus. Vestibulum condimentum cursus quam gravida vestibulum. Ut ut lacus accumsan, porta ante in, sodales felis. Vivamus pellentesque faucibus risus vel gravida. Quisque non risus tellus. Vestibulum auctor justo ut libero mollis dignissim. Morbi vitae nunc tortor. Pellentesque sagittis arcu vitae orci varius fringilla. Nam elementum nisi scelerisque porttitor vehicula.</p>
 
             <p>Proin vitae tellus feugiat, pellentesque ex eu, porta orci. Donec nec enim accumsan, lacinia ligula nec, varius dolor. Sed vitae interdum urna, eget dapibus lacus. Ut vehicula ante vitae suscipit ultrices. Etiam nec arcu justo. Nam bibendum non tortor sit amet interdum. Ut quis mauris id orci auctor sollicitudin. Nulla pretium fermentum eros, eu gravida nulla rhoncus at. Nullam sollicitudin porta turpis, at viverra eros lacinia volutpat. Nunc ut lectus quis velit malesuada finibus. Sed sem magna, fermentum in condimentum vel, vestibulum vitae dui. Quisque venenatis mauris nec malesuada aliquam. Nam molestie dolor ac urna malesuada, sit amet convallis ante fringilla.</p>
@@ -45,7 +53,7 @@
         </v-container>
 
         <v-sheet color="grey lighten-2">
-            <v-container>
+            <v-container class="constrainSize">
                 <p>Integer congue rutrum lacinia. Nam mollis ex sit amet ante suscipit mollis. Morbi malesuada lorem non nisl pretium tincidunt. Sed elementum augue non erat sagittis, sed scelerisque arcu venenatis. Donec fermentum commodo lectus, sed dignissim enim tempus at. In est libero, mollis non scelerisque et, egestas eget est. Fusce euismod metus et sapien hendrerit, at sollicitudin mauris ullamcorper. Aenean cursus pulvinar nulla in gravida.</p>
 
                 <p>Mauris sit amet nulla viverra, fermentum quam vitae, ornare sapien. Ut tempor, purus at aliquam luctus, diam libero pulvinar enim, eget congue ex lacus posuere neque. Pellentesque mollis rhoncus ullamcorper. Aliquam tellus felis, vestibulum ut gravida non, tincidunt ac sapien. Aenean molestie ex et ligula dictum sagittis. Nullam ullamcorper commodo turpis vitae facilisis. Suspendisse potenti. Cras maximus vel mi in porttitor. Praesent gravida tincidunt fermentum. Sed auctor nulla sed lacus eleifend consequat. Proin lacinia nisl ac porttitor gravida.</p>
@@ -54,7 +62,7 @@
             </v-container>
         </v-sheet>
 
-        <v-container>
+        <v-container class="constrainSize">
             <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur vel gravida leo. Nam ornare metus in risus dapibus, nec bibendum nunc dictum. Nulla interdum commodo enim, quis tempus ipsum placerat sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pretium hendrerit nulla, accumsan luctus lectus tempor id. Duis luctus nulla et metus porttitor, sed egestas turpis tempor. Morbi augue neque, eleifend at pharetra non, hendrerit a tortor. Curabitur vitae euismod dui. Sed ultricies ipsum quis neque vestibulum pellentesque. Nullam at justo lorem.</p>
 
             <p>Sed tempor est diam, suscipit placerat purus bibendum non. Integer a felis interdum, imperdiet nisl at, maximus justo. Curabitur ac tortor porta, porta nibh sed, suscipit mi. Nam at ipsum eget orci elementum consequat ut a turpis. Mauris pellentesque vulputate turpis, vel posuere ipsum maximus ut. Donec nec vestibulum odio. Phasellus sit amet risus enim. Maecenas porta nisi aliquet neque ultricies, vitae vehicula lacus consequat. Proin sed augue sit amet tortor efficitur congue. Nam fermentum lorem dolor, sit amet imperdiet arcu laoreet id. Duis et nibh gravida, porttitor elit non, dictum dui. Phasellus pellentesque lectus nec pretium luctus.</p>
@@ -63,7 +71,7 @@
         </v-container>
 
         <v-sheet color="grey lighten-2">
-            <v-container>
+            <v-container class="constrainSize">
                 <p>Duis lobortis augue eget est malesuada porttitor. Nam semper dui felis, ut dictum justo mollis et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus non felis imperdiet, scelerisque mi quis, luctus lacus. Nulla facilisi. In hac habitasse platea dictumst. Fusce pellentesque augue dolor, ut bibendum nulla condimentum non. Nullam ullamcorper gravida sapien, nec sodales ex dictum quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
 
                 <p>Donec mattis augue sed bibendum dignissim. Integer libero neque, euismod vel urna et, condimentum placerat nisi. Ut ornare pretium interdum. Phasellus ac arcu et est lobortis venenatis ut vitae ex. Phasellus malesuada erat ligula, vehicula ultrices elit dignissim non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum nulla justo, varius non erat ut, pellentesque eleifend lorem. Etiam vitae sollicitudin mi. Vestibulum porta, nibh ac convallis mollis, orci tortor pretium lectus, quis cursus lacus sapien a nulla. Curabitur ut feugiat purus, sed convallis justo. In rutrum commodo dolor non consequat. Nullam consequat dolor quis mauris laoreet, eget luctus augue semper. Nam interdum eu lectus eu condimentum.</p>
@@ -72,7 +80,7 @@
             </v-container>
         </v-sheet>
 
-        <v-container>
+        <v-container class="constrainSize">
             <p>Nullam posuere sagittis odio in luctus. Donec sollicitudin, tellus in luctus porta, sapien erat iaculis nulla, ultricies scelerisque nisl arcu id lacus. Phasellus sed ex non libero commodo molestie eget sed enim. Nulla facilisi. Fusce at dolor ex. Mauris tellus mi, gravida eget nulla eu, maximus facilisis urna. Phasellus imperdiet efficitur dui quis posuere. Nulla porta dignissim lacinia. Pellentesque hendrerit varius felis at sollicitudin.</p>
 
             <p>Curabitur eu quam sit amet metus dictum pulvinar non nec massa. Integer viverra velit orci, sed aliquet tellus ultricies id. Morbi sed placerat arcu, ut hendrerit arcu. Mauris tempor lacinia est, id gravida quam vulputate rutrum. Sed sit amet neque ac mi pellentesque tincidunt quis et magna. Curabitur eget dapibus ipsum, id ornare risus. Donec sit amet massa lobortis, posuere lorem eget, consectetur massa. Aenean eleifend, massa accumsan mattis ultricies, nisl risus venenatis massa, at interdum nisi massa quis sem. Maecenas vulputate sit amet magna eu aliquam. Aliquam efficitur orci sed tortor rutrum dictum. Fusce consectetur enim at tempor pharetra. Mauris ultricies nibh tempus purus ultrices finibus. Fusce tempus turpis eu orci volutpat aliquam.</p>
@@ -81,14 +89,14 @@
         </v-container>
 
         <v-sheet color="grey lighten-2">
-            <v-container>
+            <v-container class="constrainSize">
                 <p>Nullam tortor est, convallis ac augue a, viverra dignissim est. Aenean pulvinar dictum lacus et blandit. Sed mattis, leo at varius dictum, nibh purus varius leo, non hendrerit magna dui id orci. Sed quis lectus purus. Etiam consectetur dapibus justo a tincidunt. Donec placerat convallis porta. Donec in sagittis nisi, eu fermentum eros. Sed maximus volutpat dolor non consequat. Suspendisse lobortis eu sapien non dapibus. Praesent elementum tortor arcu, in tempor eros luctus eu. Phasellus convallis vitae dui nec scelerisque. Sed eget tincidunt leo, sed dapibus neque. Pellentesque eget porttitor libero. Integer sapien tortor, finibus cursus ex vitae, hendrerit pharetra ligula. Nullam vitae consequat mi.</p>
 
                 <p>Integer et felis quis diam placerat efficitur. Aliquam ac eros elementum, molestie lacus at, hendrerit lectus. Fusce id nisl dictum mi bibendum tincidunt. Phasellus vel tellus et lectus tincidunt ultrices. Nam non odio hendrerit ante volutpat auctor id sit amet metus. Cras vel nulla erat. Donec tincidunt pellentesque leo a maximus. In hac habitasse platea dictumst. Suspendisse efficitur malesuada vestibulum. Vivamus posuere varius sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
             </v-container>
         </v-sheet>
 
-        <v-container>
+        <v-container class="constrainSize">
             <p>In tristique, est porta lobortis pharetra, magna leo sagittis elit, a faucibus justo nisi at velit. Duis interdum erat eu mauris laoreet volutpat. Integer tortor velit, dictum vel ligula sit amet, ultrices placerat mi. Etiam in fermentum enim. Aenean in ullamcorper nisi. Quisque ligula libero, consequat in finibus cursus, lobortis at nisi. Integer ut libero eu tellus vehicula posuere a vel nunc.</p>
 
             <p>Etiam facilisis sodales justo ac ullamcorper. Quisque ut lectus sit amet lacus ornare euismod eget at nisl. Phasellus quam elit, bibendum ac erat aliquet, sodales dignissim mi. Sed vitae urna vel urna ultricies posuere at auctor tellus. Aenean in lacus urna. Etiam efficitur urna non eros accumsan, eget suscipit lacus convallis. Nulla vel lobortis purus, a ornare magna.</p>
@@ -121,3 +129,9 @@ export default class IndexPage extends Vue {
         ]
 }
 </script>
+
+<style lang="scss" scoped>
+    .constrainSize {
+        max-width: 1190px;
+    }
+</style>
